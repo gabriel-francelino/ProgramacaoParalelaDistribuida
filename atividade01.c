a459f2fbc493022c824cad94e8d5da880783f807
Gabriel Francelino <gabriel.francelino@sou.unifal-mg.edu.br>
2023-09-14 17:39:15 -0300

    Refazendo o código por que estava dando erro

--- atividade01.c
+++ atividade01.c
@@ -4,22 +4,10 @@
 #include <time.h>
 #include <unistd.h>
 
-#define MASTER_RANK 0
-#define TAG_TASK 0
-#define TAG_RESULT 1
-#define TAG_FINISH 10
+#define MASTER_RANK 10
 
-// Função para gerar números aleatórios entre 0 e 99
-int generateRandomNumber() {
-    return rand() % 100;
-}
-
-// Função para imprimir um vetor de inteiros
-void printIntArray(int* arr, int size) {
-    for (int i = 0; i < size; i++) {
-        printf("%d ", arr[i]);
-    }
-    printf("\n");
+int generateTask(){
+    return (rand() % 4); // Gerando tags de terefa de 0 à 3
 }
 
 // Função para calcular a soma de um vetor de inteiros
@@ -75,92 +63,52 @@ int calculateMedian(int* arr, int size) {
 }
 
 int main(int argc, char** argv) {
-    int num_processes, rank;
-    MPI_Init(&argc, &argv);
-    MPI_Comm_size(MPI_COMM_WORLD, &num_processes);
+    int world_size, rank;
+    MPI_Init(NULL, NULL);
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-    srand(time(NULL) + rank);
+    srand(time(NULL));
 
     if (rank == MASTER_RANK) {
-        int operation = 0;
-        int num_tasks = 0;
-        int total_tasks = 5; // Número total de tarefas a serem executadas
+        int tag_task = 0; // TAG que vai definir a tarefa a ser feita
+        int total_task = 50;
 
-        while (operation < 4) { // precisa mudar os parametros, num_task ta errado
-            // Gera valores aleatórios e envia para os escravos com uma tag aleatória
-            int num_values = rand() % 1001 + 1000; // Entre 1000 e 2000
-            operation = rand() % total_tasks; // Operação aleatória (0 a 4)
-            printf("Operação agora é %d\n", operation);
-            // operation = (operation >= 4) ? 10 : operation;
-            int values[num_values];
-            for (int i = 0; i < num_values; i++) {
-                values[i] = generateRandomNumber();
+        // Enviando primeira rodada de tarefas
+        for (int dest = 0; dest < world_size; dest++)
+        {
+            int numbers_amount = rand() % 1001 + 1000; // Gerando números aleatórios entre 1000 e 2000
+            int tag_task = generateTask; // Gerando task aleatória
+            int numbers[numbers_amount]; // Vetor de números aleatórios
+
+            for (int i = 0; i < numbers_amount; i++) {
+                numbers[i] = rand() % 100; // Gerando números de 0 a 99
             }
-            int task_tag = rand() % num_processes; // Tag aleatória para a tarefa
-            
-            // Envie a tarefa para o escravo com a tag aleatória
-            MPI_Send(&num_values, 1, MPI_INT, task_tag, TAG_TASK, MPI_COMM_WORLD);
-            MPI_Send(&operation, 1, MPI_INT, task_tag, TAG_TASK, MPI_COMM_WORLD);
-            MPI_Send(values, num_values, MPI_INT, task_tag, TAG_TASK, MPI_COMM_WORLD);
 
-            printf("Tarefa #%d - Enviando para Escravo %d:\n", num_tasks, task_tag);
-            // printf("Valores: ");
-            // printIntArray(values, num_values);
-            printf("Operação: %d\n\n", operation);
-
-            num_tasks++;
-
-            // Recebe o resultado do escravo
-            int result;
-            MPI_Recv(&result, 1, MPI_INT, task_tag, TAG_RESULT, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-            printf("Resultado do Escravo %d: %d\n", task_tag, result);
+            MPI_Send(&numbers_amount, 1, MPI_INT, dest, tag_task, MPI_COMM_WORLD);
+            MPI_Send(numbers, numbers_amount, MPI_INT, dest, tag_task, MPI_COMM_WORLD);
+            total_task--;
         }
+        
 
-        // Envia mensagem de finalização para os escravos
-        for (int i = 1; i < num_processes; i++) {
-            int stop = 10;
-            MPI_Send(&stop , 1, MPI_INT, i, TAG_FINISH, MPI_COMM_WORLD);
-        }
+        // while (tag_task != 10)
+        // {
+        //     int numbers_amount = rand() % 1001 + 1000; // Gerando números aleatórios entre 1000 e 2000
+        //     int tag_task = generateTask; // Gerando task aleatória
+        //     int numbers[numbers_amount]; // Vetor de números aleatórios
+
+        //     for (int i = 0; i < numbers_amount; i++) {
+        //         numbers[i] = rand() % 100; // Gerando números de 0 a 99
+        //     }
+        // }
+        
+        
     } else {
-        int operation = 0;
-        while (operation < 4) {
-            int num_values, operation;
-            MPI_Recv(&num_values, 1, MPI_INT, MASTER_RANK, TAG_TASK, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-            MPI_Recv(&operation, 1, MPI_INT, MASTER_RANK, TAG_TASK, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-            
-            if (operation == TAG_FINISH) {
-                break;
-            }
+        // Recebendo primeira leva de tarefas
+        int recv_numbers_amount;
+        //MPI_Recv(&recv_numbers_amount, 1, MPI_INT, MASTER_RANK, tag_task, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 
-            int values[num_values];
-            MPI_Recv(values, num_values, MPI_INT, MASTER_RANK, TAG_TASK, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-
-            // Realiza a operação solicitada
-            int result = 0;
-            switch (operation) {
-                case 0: // Soma
-                    result = calculateSum(values, num_values);
-                    break;
-                case 1: // Média
-                    result = calculateAverage(values, num_values);
-                    break;
-                case 2: // Maior valor
-                    result = findMax(values, num_values);
-                    break;
-                case 3: // Mediana
-                    result = calculateMedian(values, num_values);
-                    break;
-                default:
-                    break;
-            }
-
-            // Aguarde um tempo para simular a execução
-            sleep(1);
-
-            // Envie o resultado de volta para o mestre
-            MPI_Send(&result, 1, MPI_INT, MASTER_RANK, TAG_RESULT, MPI_COMM_WORLD);
-        }
+        
     }
 
     MPI_Finalize();
--- makefile
+++ makefile
@@ -7,4 +7,4 @@ run:
 	mpirun -np $(np) --hostfile hostfile  ./$(file)
 
 clean:
-	rm -rf 03_ping_pong 04_check_status 05_sum 06_compare_bcast 06_me_exercise 07_avg 07_me_exercise
+	rm -rf 03_ping_pong 04_check_status 05_sum 06_compare_bcast 06_me_exercise 07_avg 07_me_exercise atividade01
