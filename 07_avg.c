5dcb25efe7e2d2b485d8fd8ce7d8f099875e4e7f
Gabriel Francelino <gabriel.francelino@sou.unifal-mg.edu.br>
2023-09-16 20:59:15 -0300

    Comentando o código para melhor entendimento

--- 07_avg.c
+++ 07_avg.c
@@ -1,19 +1,11 @@
-// Author: Wes Kendall
-// Copyright 2012 www.mpitutorial.com
-// This code is provided freely with the tutorials on mpitutorial.com. Feel
-// free to modify it for your own use. Any distribution of the code must
-// either provide a link to www.mpitutorial.com or keep this header intact.
-//
-// Program that computes the average of an array of elements in parallel using
-// MPI_Scatter and MPI_Gather
-//
+// Inclui bibliotecas necessárias
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 #include <mpi.h>
 #include <assert.h>
 
-// Creates an array of random numbers. Each number has a value from 0 - 1
+// Cria um array de números aleatórios. Cada número está no intervalo de 0 a 1.
 float *create_rand_nums(int num_elements) {
   float *rand_nums = (float *)malloc(sizeof(float) * num_elements);
   assert(rand_nums != NULL);
@@ -24,7 +16,7 @@ float *create_rand_nums(int num_elements) {
   return rand_nums;
 }
 
-// Computes the average of an array of numbers
+// Calcula a média de um array de números
 float compute_avg(float *array, int num_elements) {
   float sum = 0.f;
   int i;
@@ -35,44 +27,45 @@ float compute_avg(float *array, int num_elements) {
 }
 
 int main(int argc, char** argv) {
+  // Verifica se o programa foi chamado com o número correto de argumentos
   if (argc != 2) {
-    fprintf(stderr, "Usage: avg num_elements_per_proc\n");
+    fprintf(stderr, "Usage: ./avg num_elements_per_proc\n");
     exit(1);
   }
 
+  // Lê o número de elementos por processo da linha de comando
   int num_elements_per_proc = atoi(argv[1]);
-  // Seed the random number generator to get different results each time
+  
+  // Inicializa o gerador de números aleatórios com uma semente baseada no tempo
   srand(time(NULL));
 
+  // Inicializa MPI
   MPI_Init(NULL, NULL);
 
+  // Obtém o número do processo atual e o número total de processos
   int world_rank;
   MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
   int world_size;
   MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 
-  // Create a random array of elements on the root process. Its total
-  // size will be the number of elements per process times the number
-  // of processes
+  // Cria um array de números aleatórios no processo raiz (rank 0)
   float *rand_nums = NULL;
   if (world_rank == 0) {
     rand_nums = create_rand_nums(num_elements_per_proc * world_size);
   }
 
-  // For each process, create a buffer that will hold a subset of the entire
-  // array
+  // Cada processo cria um buffer para armazenar um subconjunto dos números aleatórios
   float *sub_rand_nums = (float *)malloc(sizeof(float) * num_elements_per_proc);
   assert(sub_rand_nums != NULL);
 
-  // Scatter the random numbers from the root process to all processes in
-  // the MPI world
+  // Distribui os números aleatórios do processo raiz para todos os processos no mundo MPI
   MPI_Scatter(rand_nums, num_elements_per_proc, MPI_FLOAT, sub_rand_nums,
               num_elements_per_proc, MPI_FLOAT, 0, MPI_COMM_WORLD);
 
-  // Compute the average of your subset
+  // Calcula a média do subconjunto
   float sub_avg = compute_avg(sub_rand_nums, num_elements_per_proc);
 
-  // Gather all partial averages down to the root process
+  // Coleta todas as médias parciais no processo raiz
   float *sub_avgs = NULL;
   if (world_rank == 0) {
     sub_avgs = (float *)malloc(sizeof(float) * world_size);
@@ -80,26 +73,25 @@ int main(int argc, char** argv) {
   }
   MPI_Gather(&sub_avg, 1, MPI_FLOAT, sub_avgs, 1, MPI_FLOAT, 0, MPI_COMM_WORLD);
 
-  // Now that we have all of the partial averages on the root, compute the
-  // total average of all numbers. Since we are assuming each process computed
-  // an average across an equal amount of elements, this computation will
-  // produce the correct answer.
+  // Agora que temos todas as médias parciais no processo raiz, calculamos a média total
   if (world_rank == 0) {
     float avg = compute_avg(sub_avgs, world_size);
     printf("Avg of all elements is %f\n", avg);
-    // Compute the average across the original data for comparison
+    // Calcula a média dos dados originais para comparação
     float original_data_avg =
       compute_avg(rand_nums, num_elements_per_proc * world_size);
     printf("Avg computed across original data is %f\n", original_data_avg);
   }
 
-  // Clean up
+  // Limpeza de memória
   if (world_rank == 0) {
     free(rand_nums);
     free(sub_avgs);
   }
   free(sub_rand_nums);
 
+  // Sincronização para garantir que todos os processos cheguem a este ponto
   MPI_Barrier(MPI_COMM_WORLD);
+  // Finaliza MPI
   MPI_Finalize();
 }
